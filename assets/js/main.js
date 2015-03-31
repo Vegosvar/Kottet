d456f19177b182bac6c5c525ae0e1b1821d30839
PerLoov <per.loov@gmail.com>
2015-03-31 23:34:13 +0200

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,21 @@ $(document).ready(function () {
         $('.iss').removeClass( "functional" ).addClass( "houston_we_have_a_problem" );
     })
 
+    $( '.butterfly' ).click(function () {
+        $('.butterfly').addClass( "active" );
+        $('.butterfly_shadow').addClass( "active" );
+    })
+
+    $( '.orange' ).click(function () {
+        $('.orange').addClass( "hide" );
+        $('.orange_eaten').addClass( "show" );
+    })
+    $( '.orange_eaten' ).click(function () {
+        $('.orange_eaten').removeClass( "show" );
+        $('.orange_gone').addClass( "show" );
+    })
+
+
 })
 
 /*
