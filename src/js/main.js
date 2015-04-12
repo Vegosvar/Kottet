9e2692f6043ef62577f65b630b49c2275c06328a
DMarby <david@dmarby.se>
2015-04-12 17:36:24 +0200

    Some cleanup, add google analytics, add voting system

--- src/js/main.js
+++ src/js/main.js
@@ -35,70 +35,41 @@ $(function () {
     $('.orange_eaten').removeClass('show')
     $('.orange_gone').addClass('show')
   })
+
+  Voting.init({
+    url: 'https://voting.mjolken.se:443',
+    id: 'Kottet',
+    no_percentages: true,
+    choices: {
+      yes: {
+        button: '.btn-yes',
+        result: '.item.yes',
+        result_percentage: '.percentage.yes'
+      },
+      
+      no: {
+        button: '.btn-no',
+        result: '.item.no',
+        result_percentage: '.percentage.no'
+      },
+
+      maybe: {
+        button: '.btn-maybe',
+        result: '.item.maybe',
+        result_percentage: '.percentage.maybe'
+      }
+    },
+    
+    elements: {
+      total_votes: '.votes',
+      success: '#voting-form',
+      error: '#voting-form'
+    },
+
+    messages: {
+      total_votes: '{{VOTES}}',
+      success: '<div class="answer"><h3>Tack för din röst!</h3></div>',
+      error: '<div class="answer"><h3>Tack för din röst!</h3></div>'
+    }
+  })
 })
-
-/*
-$(document).ready(function () {
-    var votes = null
-
-    $('#ja').click(function () {
-        socket.emit('vote', { vote: 'yes' })
-    })
-
-    $('#nej').click(function () {
-        socket.emit('vote', { vote: 'no' })
-    })
-
-    $('#kanske').click(function () {
-        socket.emit('vote', { vote: 'maybe'})
-    })
-
-    var displayDone = function () {
-      $('.votingarea').html('<h3>Tack för din röst!</h3>')
-    }
-
-    var displayError = function (error) {
-        $('.votingarea').html('<h3>Du har redan röstat!</h3>')
-    }
-
-    var displayResults = function () {
-        $('.totalVotes').html('<span class="glyphicon glyphicon-signal"></span>  ' + totalVotes + ' röster')
-        var yesPercent = Math.floor(votes.yes / totalVotes * 100)
-        var noPercent = Math.floor(votes.no / totalVotes * 100)
-        var maybePercent = Math.floor(votes.maybe / totalVotes * 100)
-        yesPercent = (isNaN(yesPercent) ? 0 : yesPercent)
-        noPercent = (isNaN(noPercent) ? 0 : noPercent)
-        maybePercent = (isNaN(maybePercent) ? 0 : maybePercent)
-        $('.yes').css('height', yesPercent + '%')
-        $('.yes .procent').text(yesPercent + '%')
-        $('.no').css('height', noPercent + '%')
-        $('.no .procent').text(noPercent + '%')
-        $('.maybe').css('height', maybePercent + '%')
-        $('.maybe .procent').text(maybePercent + '%')
-    }
-
-    var socket = io.connect('https://voting.mjolken.se:443')
-
-    socket.on('initial', function (data) {
-        if (data.voted) {
-           displayDone()
-        }
-    })
-
-    socket.on('votes', function (data) {
-        votes = data
-        totalVotes = data.no + data.yes + data.maybe
-        displayResults()
-    })
-
-    socket.on('voteresult', function (data) {
-        $.cookie('hasVoted', true)
-        if (data.error) {
-            displayError()
-        } else {
-            displayDone()
-        }
-    })
-})
-*/
-
